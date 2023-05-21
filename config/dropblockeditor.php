b6a8b9cfebe75a006fef37f26f182009a3954df6
Tomas Jönsson <tomas@undeco.se>
2023-05-21 10:53:16 +0200

    Add support for custom css for preview
    - Add new config value
    - Check if file exist else fallback to editor.css

--- config/dropblockeditor.php
+++ config/dropblockeditor.php
@@ -1,17 +1,37 @@
 <?php
 
-// config for Jeffreyvr/DropBlockEditor
 return [
     'include_js' => true,
 
     'include_css' => true,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Customize editor
+    |--------------------------------------------------------------------------
+    |
+    | preview_css - Overide with custom css for the preview.
+    | brand - Custom branding for the editor.
+    |
+    */
+
+    'preview_css' => null,
+
     'brand' => [
-        'logo' => '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">   <path stroke-linecap="round" stroke-linejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" /> </svg> ',
+        'logo' => '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">   <path stroke-linecap="round" stroke-linejoin="round" d="M21 7.5l-9-5.25L3 7.5m18 0l-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" /> </svg> ',
         'colors' => [
             'topbar_bg' => 'bg-white text-gray-800',
         ],
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Register global blocks
+    |--------------------------------------------------------------------------
+    |
+    | Available blocks if not declared in the livewire dropblockeditor component
+    |
+    */
 
     'blocks' => [
         Jeffreyvr\DropBlockEditor\Blocks\Example::class,
